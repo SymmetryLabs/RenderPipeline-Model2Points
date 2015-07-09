94309f440c2afb1edb4101d59c2eceb920993d3f
Trip Vest <trip.vest@gmail.com>
2015-07-09 14:37:20 -0700

    Debugged refactored files

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ import csv, sys
 
 
 
+# def load_spreadsheet(filename, sheet_name="Sheet1"):
 def load_spreadsheet(filename, sheet_name="Sheet1"):
 	wb = load_workbook(filename, data_only=True)
-	spresadsheet = wb[sheetname]
+	spreadsheet = wb[sheet_name]
+	return spreadsheet
 
 def cellIsCube(c):
-	"Folding Cube Asm" in c and not 'Edge Asm' in c
+	return "Folding Cube Asm" in c and not 'Edge Asm' in c
 
+def cellIsEdge(c):
+	return 'Edge Asm' in c
 
 # 
 # PROCESS SPREADSHEET INTO APPROPRIATE PYTHON DATA STRUCTURE
@@ -26,12 +30,12 @@ def process(st):
 	cube = {}
 	stripID = 0
 	leds = []
+
 	# Cycle through rows
 	for row in xrange(2, st.get_highest_row()+1):
-	# for row in range(2, 48):
 
 		# If we're at the beginning of a Cube
-		if cellIsCube(st['A'+str(row)].value):
+		if cellIsCube( st['A'+str(row)].value ):
 			if bool(cube):
 				cubes.append(cube)
 			cube = {}
@@ -51,7 +55,7 @@ def process(st):
 			# cube['id'] = 
 
 		# Colleting strip coordinates
-		elif 'Edge Asm' in st['A'+str(row)].value:
+		elif cellIsEdge( st['A'+str(row)].value ):
 			strip = {}
 
 			R0 = (st['B%d'%row].value, st['C%d'%row].value, st['D%d'%row].value)
@@ -67,32 +71,10 @@ def process(st):
 			strip['p2'] = [ st['Q%d'%row].value, st['R%d'%row].value, st['S%d'%row].value ]
 			strip['id'] = (st['A'+str(row)].value).split('-')[-1]
 			# print 'stripID = ', (st['A'+str(row)].value).split('-')[-1]
+			# print cube
+			# print cube.keys()
 			cube['strips'].append(strip)
-			# stripID+=1
 
-
-	# print cubes[0]
-	# ls = range(1,23)
-	# z = zeros( (1, 3) )
-
-	# cubePoints = []
-
-	# for l in ls:
-	# 	led = (0, 0, -l)
-	# 	cubePoints.append(led)
-
-	# for l in ls:
-	# 	led = (0, l, 0)
-	# 	cubePoints.append(led)
-
-	# for l in ls:
-	# 	led = (l, 0, 0)
-	# 	cubePoints.append(led)
-
-
-	# # cubePoints = array( [ (0, 0, 1), (0, 1, 0), (1, 0, 0) ] )
-
-	# points = empty(3)
 
 	for cubeIdx, cube in enumerate(cubes):
 		strips = cube['strips']
@@ -126,17 +108,21 @@ def process(st):
 # numpy.savetxt("Curvey Arches Structure.csv", a, delimiter=",")
 
 def output_file(leds, outfilename):	
-	with open(outfilename, 'wb') as csvfile:
+	with open('data/'+outfilename, 'wb') as csvfile:
 		fieldnames = leds[0].keys()
 		writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
 		writer.writeheader()
 		writer.writerows(leds)
+		print "Output to ", outfilename, " COMPLETE"
 
 def main(argv):
-	st = load_spreadsheet(filename=argv[0], sheet_name= argv[1] if argv[1] else None)
+	user_input = raw_input("Enter spreadsheet filename ... ")
+
+	# st = load_spreadsheet(filename=user_input, sheet_name=None)
+	st = load_spreadsheet(filename='data/'+user_input)
 	leds = process(st)
-	output_file(leds,argv[2] if argv[2] else 'out.csv')
+	output_file( leds, user_input.split('.')[0] + ".csv" )
 
 
 if __name__ == "__main__":
-    process(sys.argv)
+    main(sys.argv)
